5c6976075c47917ca37e745580b38ef91af51bde
morgan.qin <morgan.qin@watsons.com.cn>
2021-08-29 09:58:10 +0800

    print liked list

--- src/linked/reverse_linked_test.go
+++ src/linked/reverse_linked_test.go
@@ -1,20 +1,26 @@
 package linked
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestReverse(t *testing.T) {
-	list2 := &ListNode{
-		Val: 20,
-		Next: &ListNode{
-			Val:  30,
-			Next: nil,
-		},
-	}
+	list2 := new(ListNode)
+	list3 := new(ListNode)
+	list2.Val = 20
+	list2.Next = list3
+	list3.Val = 30
+
 	list1 := &ListNode{
 		Val:  10,
 		Next: list2,
 	}
-	ReverseList(list1)
+
+	list1 = ReverseList(list1)
+	for list1 != nil {
+		fmt.Printf("%+v ->", list1.Val)
+		list1 = list1.Next
+	}
+	fmt.Println()
 }
